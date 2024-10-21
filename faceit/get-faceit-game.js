8af014a07a72f73d599e074a5bbafb4921ffc1ba
Adomaitis Tadas <tadas.adomaitis@mif.stud.vu.lt>
2024-10-21 14:10:26 +0300

    faceit test

--- faceit/get-faceit-game.js
+++ faceit/get-faceit-game.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 async function getFaceitDemoFile(resourceUrl) {
-    console.log('download faceitFile');
     try {
+        console.log('download faceitFile');
         const demoFileResponse = await axios.post("https://open.faceit.com/download/v2/demos/download", { resource_url: resourceUrl }, {
             headers: {
                 'Content-Type': 'application/json',
@@ -26,25 +26,35 @@ async function getFaceitPlayerId(nickname) {
 }
 
 async function getPlayerMatchHistory(playerId) {
-    const machHistoryData = await axios.get(`https://open.faceit.com/data/v4/players/${playerId}/history`, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.FACEIT_TOKEN}`
-        }
-    });
-    console.log(machHistoryData.data.items[0].match_id);
-    return [machHistoryData.data.items[0].match_id, machHistoryData.data.items[0].game_mode];
+    try {
+        console.log('get match history');
+        const machHistoryData = await axios.get(`https://open.faceit.com/data/v4/players/${playerId}/history`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.FACEIT_TOKEN}`
+            }
+        });
+        console.log(machHistoryData.data.items[0].match_id);
+        return [machHistoryData.data.items[0].match_id, machHistoryData.data.items[0].game_mode];
+    } catch (e) {
+        console.error(e);
+    }
 }
 
 async function getMatchData(matchId) {
-    const matchData = await axios.get(`https://open.faceit.com/data/v4/matches/${matchId}`, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.FACEIT_TOKEN}`
-        }
-    });
-    console.log(matchData.data.demo_url[0]);
-    return matchData.data.demo_url[0];
+    try {
+        console.log('get match data');
+        const matchData = await axios.get(`https://open.faceit.com/data/v4/matches/${matchId}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.FACEIT_TOKEN}`
+            }
+        });
+        console.log(matchData.data.demo_url[0]);
+        return matchData.data.demo_url[0];
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 module.exports = { getFaceitDemoFile, getFaceitPlayerId, getPlayerMatchHistory, getMatchData };
